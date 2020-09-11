54bf812dab547309db97256c0b6bc7d0ea46c2d8
Witerlland <witerlland@outlook.com>
2020-09-10 22:16:13 -0300

    Reorganizado as skills e os projetos.

--- scripts.js
+++ scripts.js
@@ -81,3 +81,57 @@
     };
 
  })();
+
+// Pega os itens dentro do container com id passado
+let ListItens = document.querySelector('#multSlider');
+let SliderContainer = document.getElementById('multSlider');
+// Starta a pagina com contador zerado
+let CountItens = 0;
+// Quantidade de itens que deve aparecer na pagina
+let ItensPerPage = 6;
+// Variavel que recebe o timer
+let Timer;
+// Chama a funcao ao carregar a pagina
+startSlider();
+
+// Adiciona a funcao para limpar o timer quando o mouse esta sob
+// o elemento container do slider
+SliderContainer.addEventListener("mouseover", function(){
+    clearTimeout(Timer);
+});
+// Adiciona a funcao para voltar o timer quando o mouse sai
+// do elemento container do slider
+SliderContainer.addEventListener("mouseleave", function(){
+    Timer = setTimeout(startSlider, 4000);
+});
+
+function startSlider(){
+    clearTimeout(Timer);
+    // Pega todos os itens com a classe slide-item
+    let Itens      = ListItens.querySelectorAll(".slide-item");
+    // quantidade total de itens
+    let TotalItens = Itens.length;
+
+    // Remove a classe active de todos os itens
+    for (i = 0; i < TotalItens; ++i) {
+        Itens[i].classList.remove('active');
+    }  
+
+    for (i = 0; i < TotalItens; ++i) {
+        // Se o item estiver dentro do 'aspecto?/' adiciona a classe
+        // .active e mostra o item na tela
+        if(i >= CountItens && i <= CountItens + ItensPerPage){
+        Itens[i].classList.add('active');
+        }
+    }
+
+    // Caso o contador chegue a quantidade total de itens
+    // zera o mesmo.
+    if(CountItens > ((TotalItens -1) - ItensPerPage) ){
+        CountItens = 0;
+    }else{
+        CountItens++;
+    }
+    // Chama a funcao a cada 4 segundos
+    Timer = setTimeout(startSlider, 4000);
+}
